cdfb70af10d4cdbdb11d306f4375ce0305c5058a
Johny Marques <johnymbr@gmail.com>
2017-07-18 23:26:30 -0300

    Inclusao de mais metodos, data classe e arquivo com configuracoes para o jackson.

--- src/main/kotlin/tutorial/FirstController.kt
+++ src/main/kotlin/tutorial/FirstController.kt
@@ -2,6 +2,7 @@ package tutorial
 
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.Instant
 
 /**
  * Our very first controller
@@ -12,4 +13,16 @@ class FirstController {
     /** The ultimate answer to life, the universe and everything */
     @RequestMapping("/answer")
     fun answer() = 42
+
+    /** Get the details of a user */
+    @RequestMapping("/user")
+    fun getUser(): User {
+        val user = User(
+                username = "grahamcox",
+                screenName = "Graham",
+                email = "grahamcox@example.com",
+                registered = Instant.now()
+        )
+        return user
+    }
 }
